ce16b9f4c01ea66e733bc4665ad8815b4da55f0a
elim0050 <elim0050@student.monash.edu>
2023-04-23 14:28:29 +1000

    Board.java implemented methods to initialise the Board in terminal Position.java setup Position class attributes

--- game/src/Board.java
+++ game/src/Board.java
@@ -1,2 +1,56 @@
+import java.util.*;
+
 public class Board {
+    protected Position [][] board ;
+    protected int width;
+    protected int height;
+
+    public Board(){
+        generateBoard();
+    }
+
+    private void createBoardFromStrings(List<String> list){
+        width = list.get(0).length();
+        height= list.size();
+        board = new Position [width][height];
+        for (int x=0; x< width ;x++){
+            for (int y= 0; y< height ;y++){
+                char c = list.get(y).charAt(x);
+                board[x][y] = new Position(x,y,c);
+            }
+        }
+    }
+
+    public void generateBoard(){
+        List<String> gameBoard = Arrays.asList(
+                "*---------*---------*",
+                "|         |         |",
+                "|   *-----*-----*   |",
+                "|   |     |     |   |",
+                "|   | *---*---* |   |",
+                "|   | |       | |   |",
+                "*---* *-------* *---*",
+                "|   | |       | |   |",
+                "|   | *---*---* |   |",
+                "|   |     |     |   |",
+                "|   *-----*-----*   |",
+                "|         |         |",
+                "*---------*---------*");
+        createBoardFromStrings(gameBoard);
+    }
+
+    public Position getBoardAt(int x , int y ) {
+        return this.board[x][y];
+    }
+
+    public void display(){
+        for (int y= 0; y< height ;y++){
+            for (int x =0; x< width;x++){
+                Position l = this.getBoardAt(x,y);
+                System.out.print(l.getDispChar());
+            }
+            System.out.println("");
+        }
+    }
+
 }
