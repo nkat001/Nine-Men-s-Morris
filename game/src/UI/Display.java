a7cf1cb9bc36a381a585d628e6ecf2a9dd7011d5
userr1010 <nath0002@student.monash.edu>
2023-04-23 11:59:27 +1000

    Display class updated with lines

--- game/src/UI/Display.java
+++ game/src/UI/Display.java
@@ -1,4 +1,21 @@
 package UI;
 
-public class Display {
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JFrame {
+    private Line line = new Line();
+
+    // Get the screen size
+    int screenWidth = new JFrame().getToolkit().getScreenSize().width;
+    int screenHeight = new JFrame().getToolkit().getScreenSize().height;
+
+    public Display(){
+        setTitle("Nine Men's Morris");
+        setSize(screenWidth, screenHeight);
+        add(line);
+        setBackground(new Color(202, 213, 75));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
