b82a2108a29d9c91ebc0cd9994339fdcff83e315
userr1010 <nath0002@student.monash.edu>
2023-05-11 23:47:04 +1000

    Home page of ui created

---
+++ game/src/game/Home.java
@@ -0,0 +1,92 @@
+package game;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Home extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane pane = new StackPane();
+
+        Screen screen = Screen.getPrimary();
+        javafx.geometry.Rectangle2D bounds = screen.getVisualBounds();
+
+        double screenWidth = bounds.getWidth();
+        double screenHeight = bounds.getHeight();
+
+        Stop[] stops = new Stop[]{
+                new Stop(0, Color.LIGHTBLUE),
+                new Stop(1, Color.LIGHTGREEN),
+                new Stop(2, Color.DARKCYAN),
+        };
+        LinearGradient gradient = new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE, stops);
+        BackgroundFill fill = new BackgroundFill(gradient, null, null);
+        Background newBackground = new Background(fill);
+        pane.setBackground(newBackground);
+
+        Image image = new Image("game/board.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(3), imageView);
+        rotateTransition.setByAngle(10);
+        rotateTransition.setCycleCount(RotateTransition.INDEFINITE);
+        rotateTransition.setAutoReverse(true);
+        rotateTransition.play();
+        double imageWidth = 500;
+        double imageHeight = 500;
+        imageView.setFitWidth(imageWidth);
+        imageView.setFitHeight(imageHeight);
+        imageView.setTranslateY(50);
+        pane.getChildren().add(imageView);
+
+        Label label = new Label("Nine Men's Morris");
+        Font font = Font.font("Impact", FontWeight.BOLD, 60);
+        label.setFont(font);
+        StackPane.setAlignment(label, Pos.TOP_CENTER);
+        label.setTranslateY(20);
+
+        label.setOpacity(0);
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(4), label);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
+        pane.getChildren().add(label);
+
+        // Player Vs Player button
+        Button button = new Button("Player Vs Player");
+        button.setScaleX(2);
+        button.setScaleY(2);
+        button.setTranslateX(150);
+        pane.getChildren().add(button);
+        StackPane.setAlignment(button, Pos.CENTER_LEFT);
+
+        // Player Vs Computer button
+        Button computerButton = new Button("Player Vs Computer");
+        computerButton.setScaleX(2);
+        computerButton.setScaleY(2);
+        computerButton.setTranslateX(-150);
+        pane.getChildren().add(computerButton);
+        StackPane.setAlignment(computerButton, Pos.CENTER_RIGHT);
+
+        Scene scene = new Scene(pane, screenWidth, screenHeight);
+        stage.setTitle("Nine Men's Morris");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
